39ee27c2e94ff0835d538a9cd59d1f5f1e8630aa
Ярку <perkosrakovich.g@gmail.com>
2026-08-09 16:09:10 +0700

    Create chat_messages.sql

---
+++ chat_messages.sql
@@ -0,0 +1,6 @@
+CREATE TABLE chat_messages (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    message TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
