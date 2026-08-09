b1a6fba6317a59f8cf0e105e5becb77995241bd5
Ярку <perkosrakovich.g@gmail.com>
2026-08-09 16:12:28 +0700

    Delete chat_messages.sql

--- chat_messages.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE chat_messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    message TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
